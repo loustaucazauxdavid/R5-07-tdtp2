2606649ff92368fe55d3b67d1bdabf15266aef9b
loustaucazauxdavid <dlcazaux@iutbayonne.univ-pau.fr>
2025-09-25 14:34:20 +0200

    Initial commit

---
+++ src/main/java/com/loustaucazauxdavid/facteurspremiers/FacteursPremiers.java
@@ -0,0 +1,14 @@
+package com.loustaucazauxdavid.facteurspremiers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FacteursPremiers {
+
+//    void generate(int nb) {
+//         Set<Integer> facteursPremiers = new HashSet<>();
+//
+//
+//    }
+
+}

65be434c99abcb3b7d896ce56ca1486466ce00f9
loustaucazauxdavid <dlcazaux@iutbayonne.univ-pau.fr>
2025-09-30 08:43:09 +0200

    facteursPremiers generate

--- src/main/java/com/loustaucazauxdavid/facteurspremiers/FacteursPremiers.java
+++ src/main/java/com/loustaucazauxdavid/facteurspremiers/FacteursPremiers.java
@@ -1,14 +1,40 @@
 package com.loustaucazauxdavid.facteurspremiers;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FacteursPremiers {
 
-//    void generate(int nb) {
-//         Set<Integer> facteursPremiers = new HashSet<>();
-//
-//
-//    }
+    static List<Integer> generate(int nb) {
+         List<Integer> facteursPremiers = new ArrayList<Integer>();
+
+         if (nb == 1)
+         {
+            return facteursPremiers;
+         }
+
+         for (int i = 2; i < nb+1; i++) {
+
+             // Ajouter le nombre courant s'il est premier ET qu'il est premier avec nb
+             if (isPrime(i) && nb % i == 0) {
+                 facteursPremiers.add(i);
+             }
+         }
+
+         return facteursPremiers;
+    }
+
+    static boolean isPrime(int nb) {
+        int nbDiv = 0;
+
+        for (int i = 1; i < nb+1; i++) {
+            if (nb % i == 0) {
+                nbDiv += 1;
+            }
+        }
+
+        // Doit avoir exactement 2 diviseurs pour être premier
+        return nbDiv == 2;
+    }
 
 }
--- src/test/java/com/loustaucazauxdavid/facteurspremiers/FacteursPremiersTest.java
+++ src/test/java/com/loustaucazauxdavid/facteurspremiers/FacteursPremiersTest.java
@@ -1,7 +1,54 @@
 package com.loustaucazauxdavid.facteurspremiers;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FacteursPremiersTest {
-    
+
+    @Test
+    void generate_avec_un_renvoie_liste_vide () {
+        int nb = 1;
+
+        List<Integer> facteursPremiers = FacteursPremiers.generate(nb);
+        assertEquals (0, facteursPremiers.size());
+    }
+
+    @Test
+    void generate_avec_deux_renvoie_liste_avec_deux () {
+        int nb = 2;
+
+        List<Integer> facteursPremiersAttendus = new ArrayList<>();
+        facteursPremiersAttendus.add(2);
+
+        List<Integer> facteursPremiers = FacteursPremiers.generate(nb);
+        assertEquals (facteursPremiersAttendus, facteursPremiers);
+    }
+
+    @Test
+    void generate_avec_deux_renvoie_liste_avec_deux_trois () {
+        int nb = 6;
+
+        List<Integer> facteursPremiersAttendus = new ArrayList<>();
+        facteursPremiersAttendus.add(2);
+        facteursPremiersAttendus.add(3);
+
+        List<Integer> facteursPremiers = FacteursPremiers.generate(nb);
+        assertEquals (facteursPremiersAttendus, facteursPremiers);
+    }
+
+    @Test
+    void generate_avec_deux_renvoie_liste_avec_deux_deux_deux () {
+        int nb = 8;
+
+        List<Integer> facteursPremiersAttendus = new ArrayList<>();
+        facteursPremiersAttendus.add(2);
+
+        List<Integer> facteursPremiers = FacteursPremiers.generate(nb);
+        assertEquals (facteursPremiersAttendus, facteursPremiers);
+    }
+
 }
